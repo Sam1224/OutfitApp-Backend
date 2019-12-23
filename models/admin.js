7d1d38dc85010b2a7316ef15225745d487733f22
Qianxiong Xu <20086454@mail.wit.ie>
2019-12-23 14:14:42 +0000

    Add admin model

---
+++ models/admin.js
@@ -0,0 +1,18 @@
+/**
+ * @Author: Sam
+ * @Date: 2019/12/23
+ * @Version: 1.0
+ **/
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const admin = new Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    phone: {type: String, required: true}
+}, {
+    collection: 'admin'
+})
+
+module.exports = mongoose.model('Admin', admin)
