512e34b7a0e41c5204bdde918246198d6f3b8101
Qianxiong Xu <20086454@mail.wit.ie>
2019-12-27 16:33:15 +0000

    Add jwt to findAll method in admin router

--- routes/admin.js
+++ routes/admin.js
@@ -20,13 +20,27 @@ var statusCode = require('../public/javascripts/status')
 router.findAll = (req, res) => {
     res.setHeader('Content-Type', 'application/json')
 
-    Admin.find((err, admins) => {
-        if (err) {
-            res.send(JSON.stringify({code: statusCode.ERR_NOK, error: err}, null, 5))
-        } else {
-            res.send(JSON.stringify({code: statusCode.ERR_OK, data: admins}, null, 5))
-        }
-    })
+    // jwt
+    let token = req.body.token
+    if (!token) {
+        res.send(JSON.stringify({code: statusCode.USER_NL, message: 'Not login yet, please login'}, null, 5))
+    } else {
+        jwt.verify(token, config.superSecret, (err, decoded) => {
+            if (err) {
+                res.send(JSON.stringify({code: statusCode.ERR_NOK, error: err}, null, 5))
+            } else {
+                req.decoded = decoded
+
+                Admin.find((err, admins) => {
+                    if (err) {
+                        res.send(JSON.stringify({code: statusCode.ERR_NOK, error: err}, null, 5))
+                    } else {
+                        res.send(JSON.stringify({code: statusCode.ERR_OK, data: admins}, null, 5))
+                    }
+                })
+            }
+        })
+    }
 }
 
 module.exports = router
