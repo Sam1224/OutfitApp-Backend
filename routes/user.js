acc43b436f2fcb0a3e63f107c735119b34b2cc54
Qianxiong Xu <20086454@mail.wit.ie>
2020-04-18 14:47:10 +0100

    Test DELETE /user/retrieval/delete

--- routes/user.js
+++ routes/user.js
@@ -595,7 +595,7 @@ router.deleteRetrieval = (req, res) => {
             if (err) {
                 res.send(JSON.stringify({code: statusCode.ERR_NOK, error: err}, null, 5))
             } else {
-                if (user.length === 0) {
+                if (user === null || user.length === 0) {
                     res.send(JSON.stringify({
                         code: statusCode.USER_NE,
                         message: 'The user not exists'
